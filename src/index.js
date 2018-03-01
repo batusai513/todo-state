55a560364ec980fa1a8157db80a8d5b4966c17a1
Richard Roncancio <batusai513@msn.com>
2018-03-01 17:54:53 -0500

    ordered files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './week4/Todo.css';
-
-import Tarea from './week3/Forms';
-import TodoApp from './week4/TodoApp';
-import MoviesApp from './week5/Movies';
+import TodoApp from './components/TodoApp';
+import './components/Todo.css';
 
 ReactDOM.render(
   <TodoApp />,
