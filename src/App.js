f8e96b94eb81039de52ca13ad59f287a591d4536
Richard Roncancio <batusai513@msn.com>
2018-03-05 15:29:32 -0500

    children props

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component, createElement } from "react";
 import P from "prop-types";
 
 //Crear componente como funcion (las propiedades llegan como parametros de la funcion)
+// la propiedad children es especial ya que no la defines directamente en el objeto de props
 function Title(props) {
-  return React.createElement(props.level, null, props.title);
+  return React.createElement(props.level, null, props.children);
 }
 
 // defines que recibe un componente, que tipo y si es obligatorio mandarlo
 // https://reactjs.org/docs/typechecking-with-proptypes.html
 Title.propTypes = {
-  title: P.string,
+  children: P.node,
   level: P.oneOf(["h1", "h2", "h3", "h4", "h5", "h6"])
 };
 
@@ -20,6 +21,10 @@ Title.defaultProps = {
 //Crear componente como clase (las propiedades estan en this.props);
 export default class App extends React.Component {
   render() {
-    return createElement(Title, { title: "hola mundo" });
+    return createElement(
+      Title,
+      {},
+      React.createElement("span", null, "hola mundo")
+    );
   }
 }
