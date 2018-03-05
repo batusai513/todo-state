e2595d4ea531ebf54a259aad6e0db8c12df29ebe
Richard Roncancio <batusai513@msn.com>
2018-03-05 14:54:08 -0500

    Elementos y components de react

--- src/App.js
+++ src/App.js
@@ -1,48 +1,23 @@
 import React, { Component } from "react";
 
-export default function App() {
-  return (
-    <section className="todoapp">
-      <header className="header">
-        <h1>todos</h1>
-        <input
-          className="new-todo"
-          placeholder="What needs to be done?"
-          autoFocus=""
-        />
-      </header>
-      <section className="main">
-        <input className="toggle-all" type="checkbox" />
-        <label htmlFor="toggle-all">Mark all as complete</label>
-        <ul className="todo-list">
-          <li className="completed">
-            <div className="view">
-              <input className="toggle" type="checkbox" />
-              <label>asda232</label>
-              <button className="destroy" />
-            </div>
-          </li>
-        </ul>
-      </section>
-      <footer className="footer">
-        <span className="todo-count">
-          <strong>1</strong> item left
-        </span>
-        <ul className="filters">
-          <li>
-            <a href="#/" className="selected">
-              All
-            </a>
-          </li>
-          <li>
-            <a href="#/active">Active</a>
-          </li>
-          <li>
-            <a href="#/completed">Completed</a>
-          </li>
-        </ul>
-        <button className="clear-completed" />
-      </footer>
-    </section>
-  );
+
+//Crear componente como funcion (las propiedades llegan como parametros de la funcion)
+// export default function App() {
+//   return React.createElement(
+//     "div",
+//     null,
+//     React.createElement("h1", null, Hola)
+//   );
+// }
+
+
+//Crear componente como clase (las propiedades estan en this.props);
+export default class App extends React.Component {
+  render() {
+    return React.createElement(
+      "div",
+      null,
+      React.createElement("h1", null, 'Hola')
+    );
+  }
 }
