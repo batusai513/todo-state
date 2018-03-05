b8bdc9b7c754786d85e56f15559ec9f5806dc687
Richard Roncancio <batusai513@msn.com>
2018-03-05 15:05:57 -0500

    multiples elementos y propiedades de los elementos de react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-
+import React, { Component, createElement } from "react";
 
 //Crear componente como funcion (las propiedades llegan como parametros de la funcion)
 // export default function App() {
@@ -10,14 +9,18 @@ import React, { Component } from "react";
 //   );
 // }
 
-
 //Crear componente como clase (las propiedades estan en this.props);
 export default class App extends React.Component {
   render() {
-    return React.createElement(
+    return createElement(
       "div",
+      { className: "main-box", style: { backgroundColor: 'aquamarine' } },
+      createElement("h1", { className: "title" }, "Hola"),
+      ['hola', 'adios'],
       null,
-      React.createElement("h1", null, 'Hola')
+      false,
+      //{} no puedes pasar objetos o undefined
+      createElement('input', { type: 'text', autoFocus: true })
     );
   }
 }
