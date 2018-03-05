c63f7ada7da2410cd7f51d6f3ac988bc6a5a5da8
Richard Roncancio <batusai513@msn.com>
2018-03-05 15:24:48 -0500

    propiedades de Componentes, proTypes y defaultProps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { Component, createElement } from "react";
+import P from "prop-types";
 
 //Crear componente como funcion (las propiedades llegan como parametros de la funcion)
-// export default function App() {
-//   return React.createElement(
-//     "div",
-//     null,
-//     React.createElement("h1", null, Hola)
-//   );
-// }
+function Title(props) {
+  return React.createElement(props.level, null, props.title);
+}
+
+// defines que recibe un componente, que tipo y si es obligatorio mandarlo
+// https://reactjs.org/docs/typechecking-with-proptypes.html
+Title.propTypes = {
+  title: P.string,
+  level: P.oneOf(["h1", "h2", "h3", "h4", "h5", "h6"])
+};
+
+Title.defaultProps = {
+  level: "h1"
+};
 
 //Crear componente como clase (las propiedades estan en this.props);
 export default class App extends React.Component {
   render() {
-    return createElement(
-      "div",
-      { className: "main-box", style: { backgroundColor: 'aquamarine' } },
-      createElement("h1", { className: "title" }, "Hola"),
-      ['hola', 'adios'],
-      null,
-      false,
-      //{} no puedes pasar objetos o undefined
-      createElement('input', { type: 'text', autoFocus: true })
-    );
+    return createElement(Title, { title: "hola mundo" });
   }
 }
