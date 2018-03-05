7c34a49f6768dfac0a81177cafdec2d7934821ac
Richard Roncancio <batusai513@msn.com>
2018-03-05 15:49:16 -0500

    estado: parte 1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import P from "prop-types";
 
 //Crear componente como funcion (las propiedades llegan como parametros de la funcion)
 // la propiedad children es especial ya que no la defines directamente en el objeto de props
+// Las propiedades de un componente de react son inmutables, no se pueden mutar de manera directa
 function Title(props) {
   return React.createElement(props.level, null, props.children);
 }
@@ -18,13 +19,32 @@ Title.defaultProps = {
   level: "h1"
 };
 
-//Crear componente como clase (las propiedades estan en this.props);
+// Crear componente como clase (las propiedades estan en this.props);
+// el estado del componente es almanecenado en la propiedad state, el estado solo se puede declarar en componentes tipo clase.
+// Cuando se muta el estado utilizando el metodo setState, se vuelve a renderizar ese componente y el arbol de componentes que estan por debajo de este
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    };
+  }
+
+  // metodos del ciclo de vida: se llaman en partes claves en el ciclo de vida del componente
+
+  componentDidMount() {
+    setInterval(function() {
+      this.setState({
+        text: Math.random()
+      });
+    }.bind(this), 1000);
+  }
+
   render() {
     return createElement(
       Title,
       {},
-      React.createElement("span", null, "hola mundo")
+      React.createElement("span", null, this.state.text)
     );
   }
 }
