e409e29f71e4ae304a1a6121b548102a64ba1c14
Marcela Garizao <march@ip-10-0-23-145.sa-east-1.compute.internal>
2018-03-06 11:15:08 -0500

    homework 1.0: convert inline HTML to components

--- src/App.js
+++ src/App.js
@@ -8,6 +8,65 @@ function Title(props) {
   return React.createElement(props.level, null, props.children);
 }
 
+function Header(props){
+ return createElement(
+    'header',
+    {className: 'header'},
+    createElement('h1', null, 'todos'),
+    createElement(
+      'input',
+      {className: 'new-todo', placeholder: 'What needs to be done?', autoFocus: true}
+    )
+ );
+}
+
+function Body(props){
+  return createElement(
+    'section',
+    {className: 'main'},
+    createElement(
+      'input',
+      {className: 'toogle-all', type: 'checkbox'}
+    ),
+    createElement(
+      'label',
+      {htmlFor: 'toggle-all'},
+      'Mark all as complete'
+    ),
+    createElement(
+      'ul',
+      {className: 'todo-list'},
+      createElement(
+        'li',
+        {className: 'completed'},
+        createElement(
+          'div',
+          {className: 'view'},
+          createElement('input', {className: 'toogle', type: 'checkbox'}),
+          createElement('label', null, 'AKA'),
+          createElement('button', {className: 'destroy'})
+        )
+      )
+    )
+  );
+}
+
+function Footer(props){
+  return createElement(
+    'footer',
+    {className: 'footer'},
+    createElement('span', {className: 'todo-count'}, createElement('strong', null, '1'), 'item left'),
+    createElement(
+      'ul',
+      {className: 'filters'},
+      createElement('li', null, createElement('a', {className: 'selected', href: '#'}, 'All')),
+      createElement('li', null, createElement('a', {href: '#/active'}, 'Active')),
+      createElement('li', null, createElement('a', {href: '#/completed'}, 'Completed')),
+      createElement('button', {className: 'clear-completed'}, '')
+    )
+  );
+}
+
 // defines que recibe un componente, que tipo y si es obligatorio mandarlo
 // https://reactjs.org/docs/typechecking-with-proptypes.html
 Title.propTypes = {
@@ -39,9 +98,11 @@ export default class App extends React.Component {
 
   render() {
     return createElement(
-      Title,
-      {},
-      React.createElement("span", null, this.state.text)
+      'section',
+      {className: 'todoapp'},
+      createElement(Header),
+      createElement(Body),
+      createElement(Footer)
     );
   }
 }
