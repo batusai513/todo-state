58934d7d2393f31fabdfdefc0e71353e45cc80e3
Richard Roncancio <batusai513@msn.com>
2018-03-06 16:20:08 -0500

    changes to JSX

--- src/App.js
+++ src/App.js
@@ -8,98 +8,67 @@ function Title(props) {
   return React.createElement(props.level, null, props.children);
 }
 
-function Header(props){
- return createElement(
-    'header',
-    {className: 'header'},
-    createElement('h1', null, 'todos'),
-    createElement(
-      'input',
-      {className: 'new-todo', placeholder: 'What needs to be done?', autoFocus: true}
-    )
- );
-}
-
-function Body(props){
-  return createElement(
-    'section',
-    {className: 'main'},
-    createElement(
-      'input',
-      {className: 'toogle-all', type: 'checkbox'}
-    ),
-    createElement(
-      'label',
-      {htmlFor: 'toggle-all'},
-      'Mark all as complete'
-    ),
-    createElement(
-      'ul',
-      {className: 'todo-list'},
-      createElement(
-        'li',
-        {className: 'completed'},
-        createElement(
-          'div',
-          {className: 'view'},
-          createElement('input', {className: 'toogle', type: 'checkbox'}),
-          createElement('label', null, 'AKA'),
-          createElement('button', {className: 'destroy'})
-        )
-      )
-    )
+function Header(props) {
+  return (
+    <header className="header">
+      <h1>Todos</h1>
+      <input
+        type="text"
+        className="new-todo"
+        placeholder="What needs to be done?"
+        autoFocus={true}
+      />
+    </header>
   );
 }
 
-function Footer(props){
-  return createElement(
-    'footer',
-    {className: 'footer'},
-    createElement('span', {className: 'todo-count'}, createElement('strong', null, '1'), 'item left'),
-    createElement(
-      'ul',
-      {className: 'filters'},
-      createElement('li', null, createElement('a', {className: 'selected', href: '#'}, 'All')),
-      createElement('li', null, createElement('a', {href: '#/active'}, 'Active')),
-      createElement('li', null, createElement('a', {href: '#/completed'}, 'Completed')),
-      createElement('button', {className: 'clear-completed'}, '')
-    )
+function Body(props) {
+  return (
+    <section className="main">
+      <input className="toggle-all" type="checkbox" />
+      <label htmlFor="toggle-all">Mark all as complete</label>
+      <ul className="todo-list">
+        <li className="completed">
+          <div className="view">
+            <input className="toggle" type="checkbox" />
+            <label>asda232</label>
+            <button className="destroy" />
+          </div>
+        </li>
+      </ul>
+    </section>
   );
 }
 
-// defines que recibe un componente, que tipo y si es obligatorio mandarlo
-// https://reactjs.org/docs/typechecking-with-proptypes.html
-Title.propTypes = {
-  children: P.node,
-  level: P.oneOf(["h1", "h2", "h3", "h4", "h5", "h6"])
-};
+function Footer(props) {
+  return (
+    <footer className="footer">
+      <span className="todo-count">
+        <strong>1</strong> item left
+      </span>
+      <ul className="filters">
+        <li>
+          <a href="#/" className="selected">
+            All
+          </a>
+        </li>
+        <li>
+          <a href="#/active">Active</a>
+        </li>
+        <li>
+          <a href="#/completed">Completed</a>
+        </li>
+      </ul>
+      <button className="clear-completed" />
+    </footer>
+  );
+}
 
-Title.defaultProps = {
-  level: "h1"
-};
-
-// Crear componente como clase (las propiedades estan en this.props);
-// el estado del componente es almanecenado en la propiedad state, el estado solo se puede declarar en componentes tipo clase.
-// Cuando se muta el estado utilizando el metodo setState, se vuelve a renderizar ese componente y el arbol de componentes que estan por debajo de este
 export default class App extends React.Component {
-  // usando class fields de es2017
-  state = {
-    text: ""
-  };
-  // metodos del ciclo de vida: se llaman en partes claves en el ciclo de vida del componente
-
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        text: Math.random()
-      });
-    }, 1000);
-  }
-
   render() {
     return createElement(
-      'section',
-      {className: 'todoapp'},
+      "section",
+      { className: "todoapp" },
       createElement(Header),
       createElement(Body),
       createElement(Footer)
