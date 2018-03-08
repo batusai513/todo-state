ab889e395324da7d5795371a5cc940c699df542a
Richard Roncancio <batusai513@msn.com>
2018-03-08 17:48:53 -0500

    toggle todo

--- src/App.js
+++ src/App.js
@@ -3,30 +3,45 @@ import Header from "./components/Header"; // default y named
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 
+function todoFactory(text) {
+  return {
+    text,
+    active: false,
+    id: Date.now()
+  }
+}
+
 export default class App extends React.Component {
   state = {
     todoList: [],
-    todo: {
-      active: false,
-      text: "",
-      id: ""
-    },
     visibilityFilter: "all",
     leftItems: 0,
     toggleTodos: false
   };
 
-  addTodo = (text) => {
+  addTodo = text => {
     this.setState({
-      todoList: this.state.todoList.concat({ text, active: false, id: Date.now() })
+      todoList: this.state.todoList.concat(todoFactory(text))
     });
-  }
+  };
+
+  toggleTodoActive = id => {
+    var todoList = this.state.todoList;
+    this.setState({
+      todoList: todoList.map(todo => {
+        if (todo.id === id) {
+          return Object.assign({}, todo, { active: !todo.active });
+        }
+        return todo;
+      })
+    });
+  };
 
   render() {
     return (
       <section className="todoapp">
         <Header onAddTodo={this.addTodo} />
-        <Body />
+        <Body list={this.state.todoList} onToggleTodo={this.toggleTodoActive} />
         <Footer />
       </section>
     );
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React from "react";
+import P from "prop-types";
 
 export default function Body(props) {
+  var list = props.list;
+  var onToggleTodo = props.onToggleTodo;
   return (
     <section className="main">
       <input className="toggle-all" type="checkbox" />
       <label htmlFor="toggle-all">Mark all as complete</label>
       <ul className="todo-list">
-        <li className="completed">
-          <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>asda232</label>
-            <button className="destroy" />
-          </div>
-        </li>
+        {list.map(todo => {
+          return (
+            <li key={todo.id} className={todo.active === true ? "completed" : ""}>
+              <div className="view">
+                <input
+                  className="toggle"
+                  type="checkbox"
+                  checked={todo.active}
+                  onChange={onActiveChange(todo.id)}
+                />
+                <label>{todo.text}</label>
+                <button className="destroy" />
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
+
+  function onActiveChange(id) {
+    return function $onActiveChange(e) {
+      onToggleTodo(id);
+    };
+  }
 }
+
+Body.propTypes = {
+  list: P.array.isRequired,
+  onToggleTodo: P.func.isRequired
+};
